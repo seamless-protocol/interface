fc3d8b9d5414f84330d573a000bd95cfba1db15c
ox katsu <oxkatsudon>
2023-09-08 11:59:50 -0700

    s token to to be reverted

--- src/modules/reserve-overview/AddTokenDropdown.tsx
+++ src/modules/reserve-overview/AddTokenDropdown.tsx
@@ -183,7 +183,7 @@ export const AddTokenDropdown = ({
                   addERC20Token({
                     address: poolReserve.aTokenAddress,
                     decimals: poolReserve.decimals,
-                    symbol: `s${poolReserve.symbol}`,
+                    symbol: `a${poolReserve.symbol}`,
                     image: !/_/.test(poolReserve.symbol) ? aTokenBase64 : undefined,
                   });
                 }
@@ -192,7 +192,7 @@ export const AddTokenDropdown = ({
             >
               <TokenIcon symbol={poolReserve.iconSymbol} sx={{ fontSize: '20px' }} aToken={true} />
               <Typography variant="subheader1" sx={{ ml: 3 }} noWrap data-cy={`assetName`}>
-                {`s${poolReserve.symbol}`}
+                {`a${poolReserve.symbol}`}
               </Typography>
             </MenuItem>
           </Box>
