cf4d01e100242f54183468158f84b2468f85bc3f
ox katsu <oxkatsudon>
2023-09-09 17:20:56 -0700

    moar s token renames

--- src/modules/reserve-overview/AddTokenDropdown.tsx
+++ src/modules/reserve-overview/AddTokenDropdown.tsx
@@ -163,7 +163,7 @@ export const AddTokenDropdown = ({
           <Box>
             <Box sx={{ px: 4, pt: 3, pb: 2 }}>
               <Typography variant="secondary12" color="text.secondary">
-                <Trans>Seamless aToken</Trans>
+                <Trans>Seamless sToken</Trans>
               </Typography>
             </Box>
             <MenuItem
--- src/modules/reserve-overview/TokenLinkDropdown.tsx
+++ src/modules/reserve-overview/TokenLinkDropdown.tsx
@@ -110,7 +110,7 @@ export const TokenLinkDropdown = ({
           <Box>
             <Box sx={{ px: 4, pt: 3, pb: 2 }}>
               <Typography variant="secondary12" color="text.secondary">
-                <Trans>Seamless aToken</Trans>
+                <Trans>Seamless sToken</Trans>
               </Typography>
             </Box>
 
