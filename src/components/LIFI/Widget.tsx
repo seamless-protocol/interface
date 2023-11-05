f98adc67f7442a397c0b8196c00b6f0f6df4d037
ryan v <ryanlvv@gmail.com>
2023-11-05 15:35:41 -0800

    Dep upgrades to fix drawer

--- src/components/LIFI/Widget.tsx
+++ src/components/LIFI/Widget.tsx
@@ -1,50 +1,29 @@
-// 'use client';
-// import { LiFiWidget, WidgetConfig } from '@lifi/widget';
-// import { useMemo } from 'react';
-
-// import { WidgetEvents } from './WidgetEvents';
-
-// export const Widget = () => {
-//   const widgetConfig: WidgetConfig = useMemo(
-//     () => ({
-//       containerStyle: {
-//         border: `1px solid rgb(234, 234, 234)`,
-//         borderRadius: '16px',
-//       },
-//       integrator: 'seamless',
-//       variant: 'drawer',
-//       //BASE ChainID
-//       toChain: 8453,
-//       toToken: '0x4200000000000000000000000000000000000006',
-//     }),
-//     []
-//   );
-
-//   return (
-//     <>
-//       <WidgetEvents />
-//       <LiFiWidget integrator="seamless" config={widgetConfig} />
-//     </>
-//   );
-// };
-
-import { LiFiWidget } from '@lifi/widget';
+'use client';
+import { LiFiWidget, WidgetConfig } from '@lifi/widget';
+import { useMemo } from 'react';
 
 import { WidgetEvents } from './WidgetEvents';
 
 export const Widget = () => {
+  const widgetConfig: WidgetConfig = useMemo(
+    () => ({
+      containerStyle: {
+        border: `1px solid rgb(234, 234, 234)`,
+        borderRadius: '16px',
+      },
+      integrator: 'seamless',
+      variant: 'drawer',
+      //BASE ChainID
+      toChain: 8453,
+      toToken: '0x4200000000000000000000000000000000000006',
+    }),
+    []
+  );
+
   return (
     <>
       <WidgetEvents />
-      <LiFiWidget
-        config={{
-          containerStyle: {
-            border: `1px solid rgb(234, 234, 234)`,
-            borderRadius: '16px',
-          },
-        }}
-        integrator="seamless"
-      />
+      <LiFiWidget integrator="seamless" config={widgetConfig} />
     </>
   );
 };
