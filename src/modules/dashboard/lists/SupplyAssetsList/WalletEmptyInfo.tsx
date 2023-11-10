28072d8d0dddb4fc0ca6732353f133c73d1d9bcf
ryanimal <ryanlvv@gmail.com>
2023-11-10 14:39:40 -0800

    Add a psuedo link for LIFI

--- src/modules/dashboard/lists/SupplyAssetsList/WalletEmptyInfo.tsx
+++ src/modules/dashboard/lists/SupplyAssetsList/WalletEmptyInfo.tsx
@@ -1,6 +1,6 @@
 import { ChainId } from '@aave/contract-helpers';
 import { Trans } from '@lingui/macro';
-import { Theme } from '@mui/material';
+import { Box, Theme } from '@mui/material';
 import { SxProps } from '@mui/system';
 import { Warning } from 'src/components/primitives/Warning';
 import { useRootStore } from 'src/store/root';
@@ -17,14 +17,33 @@ type WalletEmptyInfoProps = Pick<NetworkConfig, 'bridge' | 'name'> & {
 
 export function WalletEmptyInfo({ bridge, name, chainId, icon, sx }: WalletEmptyInfoProps) {
   const network = [ChainId.avalanche].includes(chainId) ? 'Ethereum & Bitcoin' : 'Ethereum';
-
-  const trackEvent = useRootStore((store) => store.trackEvent);
+  const [trackEvent, isLifiWidgetOpen, setLifiWidgetOpen] = useRootStore((state) => [
+    state.trackEvent,
+    state.isLifiWidgetOpen,
+    state.setLifiWidgetOpen,
+  ]);
 
   return (
     <Warning severity="info" icon={icon} sx={sx}>
       {bridge ? (
         <Trans>
-          Your {name} wallet is empty. Purchase or transfer assets or use{' '}
+          Your {name} wallet is empty.{' '}
+          <Box
+            component="span"
+            onClick={() => {
+              if (!isLifiWidgetOpen) {
+                setLifiWidgetOpen(true);
+              }
+            }}
+            sx={{
+              textDecoration: 'underline',
+              cursor: isLifiWidgetOpen ? 'inherit' : 'pointer',
+              color: isLifiWidgetOpen ? 'text.disabled' : 'inherit',
+            }}
+          >
+            Purchase or transfer assets
+          </Box>{' '}
+          or use{' '}
           {
             <Link
               onClick={() => {
