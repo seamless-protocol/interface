0c04fda6758cef9bceb597ad048a12c47151dede
Wesley Frederickson <6827305+fredwes@users.noreply.github.com>
2023-11-28 11:56:43 -0800

    fix: update copy andfix pr comments

--- src/components/Subscribe/CBSubscribeButton.tsx
+++ src/components/Subscribe/CBSubscribeButton.tsx
@@ -15,51 +15,35 @@ declare global {
       }) => void;
       toggleSubscription: () => void;
     };
-    ethereum: {
-      isConnected: () => boolean;
-      // eslint-disable-next-line @typescript-eslint/no-explicit-any
-      providers: any;
-      // eslint-disable-next-line @typescript-eslint/no-explicit-any
-      setSelectedProvider: any;
-      // eslint-disable-next-line @typescript-eslint/no-explicit-any
-      request: any;
-    };
   }
 }
 
+const MODAL_BODY_TEXT = `Receive the latest updates, promotions, and alerts directly in your wallet!
+Powered by Coinbase`;
+
 export function CBSubscribeButton() {
-  const { provider } = useWeb3Context();
+  const { provider, connected, readOnlyMode } = useWeb3Context();
   const [isSubscribed, setISubscribed] = useState(false);
   const [isLoading, setIsLoading] = useState(true);
 
-  const isBrowserAndConnected = () => {
-    if (typeof window !== 'undefined' && window.ethereum) {
-      return true;
-    }
-    return false;
-  };
-
   const isCB = provider?.connection.url.includes('coinbase');
 
-  const subscribeButtonText = useMemo(() => {
-    if (isLoading) return 'Loading...';
-    return isSubscribed ? 'Unsubscribe' : 'Subscribe';
-  }, [isLoading, isSubscribed]);
+  const subscribeButtonText = isLoading ? 'Loading...' : isSubscribed ? 'Unsubscribe' : 'Subscribe';
 
   useEffect(() => {
-    if (isBrowserAndConnected() && window.CBWSubscribe) {
+    if (provider && connected && !readOnlyMode && window.CBWSubscribe) {
       window.CBWSubscribe.createSubscriptionUI({
-        partnerAddress: '0xaf2b090C37f4556BD86E3Cd74740FF0098fad3c6',
+        partnerAddress: '0x1503f06d951440fbfA211341D3399Beb3C642414',
         partnerName: 'Seamless Protocol',
-        modalTitle: 'Subscribe to Seamless Protocol',
-        modalBody: 'Hi, I am a modal body!',
+        modalTitle: 'Subscribe to Seamless updates',
+        modalBody: MODAL_BODY_TEXT,
         onSubscriptionChange: () => setISubscribed,
         onLoading: () => setIsLoading(false),
       });
     } else {
       console.error('window.CBWSubscribe is not defined');
     }
-  }, []);
+  }, [provider, connected, readOnlyMode]);
 
   const handleSubscribe = useCallback(() => {
     if (window && window.CBWSubscribe) {
