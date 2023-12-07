2f50d07e130cd07bf08ea9e4d1584b49a16c9f6d
Wesley Frederickson <6827305+fredwes@users.noreply.github.com>
2023-12-06 17:32:35 -0800

    chore: update links and addresses

--- src/ui-config/governanceConfig.ts
+++ src/ui-config/governanceConfig.ts
@@ -19,9 +19,9 @@ export const governanceConfig: GovernanceConfig = {
   chainId: ChainId.base,
   votingAssetName: 'SEAM + esSEAM',
   seamTokenAddress: '0x1C7a460413dD4e964f96D8dFC56E7223cE88CD85',
-  esSEAMTokenAddress: '0x998e44232BEF4F8B033e5A5175BDC97F2B10d5e5', // TODO(wes) mainnet
+  esSEAMTokenAddress: '0x998e44232BEF4F8B033e5A5175BDC97F2B10d5e5',
   governanceForumLink: 'https://seamlessprotocol.discourse.group',
-  governanceFAQLink: '', // TODO(wes)
+  governanceFAQLink: 'https://docs.seamlessprotocol.com/governance/governance-overview',
   governanceTallyLink: '', // TODO(wes)
   walletBalanceProvider: '0xDb0f02421f830398d7b59dae8d385e2Cd5ed5CF7',
   governanceSnapshotLink: 'https://snapshot.org/#/seamlessprotocol.eth',
