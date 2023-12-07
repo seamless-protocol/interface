a2df2473dde1cfb7c67fb8395697bdc4e55f7a0b
Wesley Frederickson <6827305+fredwes@users.noreply.github.com>
2023-12-06 22:15:06 -0800

    chore: update Tally link

--- src/ui-config/governanceConfig.ts
+++ src/ui-config/governanceConfig.ts
@@ -22,7 +22,7 @@ export const governanceConfig: GovernanceConfig = {
   esSEAMTokenAddress: '0x998e44232BEF4F8B033e5A5175BDC97F2B10d5e5',
   governanceForumLink: 'https://seamlessprotocol.discourse.group',
   governanceFAQLink: 'https://docs.seamlessprotocol.com/governance/governance-overview',
-  governanceTallyLink: '', // TODO(wes)
+  governanceTallyLink: 'https://www.tally.xyz/gov/seamless-protocol',
   walletBalanceProvider: '0xDb0f02421f830398d7b59dae8d385e2Cd5ed5CF7',
   governanceSnapshotLink: 'https://snapshot.org/#/seamlessprotocol.eth',
   addresses: {
