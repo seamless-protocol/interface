42c35ef0c8a0f9dcccbfaf3f4df07e64e6e7376e
fredwes <6827305+fredwes@users.noreply.github.com>
2023-12-07 10:27:20 -0800

    chore: update links and addresses (#18)

--- src/ui-config/governanceConfig.ts
+++ src/ui-config/governanceConfig.ts
@@ -19,10 +19,10 @@ export const governanceConfig: GovernanceConfig = {
   chainId: ChainId.base,
   votingAssetName: 'SEAM + esSEAM',
   seamTokenAddress: '0x1C7a460413dD4e964f96D8dFC56E7223cE88CD85',
-  esSEAMTokenAddress: '0x998e44232BEF4F8B033e5A5175BDC97F2B10d5e5', // TODO(wes) mainnet
+  esSEAMTokenAddress: '0x998e44232BEF4F8B033e5A5175BDC97F2B10d5e5',
   governanceForumLink: 'https://seamlessprotocol.discourse.group',
-  governanceFAQLink: '', // TODO(wes)
-  governanceTallyLink: '', // TODO(wes)
+  governanceFAQLink: 'https://docs.seamlessprotocol.com/governance/governance-overview',
+  governanceTallyLink: 'https://www.tally.xyz/gov/seamless-protocol',
   walletBalanceProvider: '0xDb0f02421f830398d7b59dae8d385e2Cd5ed5CF7',
   governanceSnapshotLink: 'https://snapshot.org/#/seamlessprotocol.eth',
   addresses: {
