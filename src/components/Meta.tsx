158058cc4dcb4617723821304ecae344eb9445e3
kitanovicd <kitanovic.danilo@outlook.com>
2023-12-11 15:02:26 +0100

    feat: remove twitter card duplicate

--- src/components/Meta.tsx
+++ src/components/Meta.tsx
@@ -23,7 +23,6 @@ export function Meta({ title, description, imageUrl, timestamp }: MetaProps) {
         content={imageUrl ? 'summary_large_image' : 'summary'}
         key="twittercard"
       />
-      <meta name="twitter:card" content="summary_large_image" key="twittercard" />
       <meta name="twitter:domain" content="http://app.seamlessprotocol.com/" key="twitterdomain" />
       <meta name="twitter:url" content="https://app.seamlessprotocol.com/" key="twitterurl" />
       <meta name="twitter:title" content={`Seamless - ${title}`} key="twittertitle" />
