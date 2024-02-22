4b96ec216075d59894cacb39e9c032bf75581717
Wesley Frederickson <6827305+fredwes@users.noreply.github.com>
2024-02-22 11:17:01 -0800

    chore: use custom icon circle stack

--- src/ui-config/menu-items/index.tsx
+++ src/ui-config/menu-items/index.tsx
@@ -1,4 +1,9 @@
-import { BookOpenIcon, CreditCardIcon, QuestionMarkCircleIcon, CircleStackIcon, GiftIcon } from '@heroicons/react/outline';
+import {
+  BookOpenIcon,
+  CreditCardIcon,
+  QuestionMarkCircleIcon,
+  GiftIcon,
+} from '@heroicons/react/outline';
 import { t } from '@lingui/macro';
 import { ReactNode } from 'react';
 import { ROUTES } from 'src/components/primitives/Link';
@@ -66,7 +71,22 @@ const moreMenuItems: MoreMenuItem[] = [
   {
     link: ROUTES.farms,
     title: t`Staking Farms`,
-    icon: <CircleStackIcon />,
+    icon: (
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        fill="none"
+        viewBox="0 0 24 24"
+        strokeWidth={1.5}
+        stroke="currentColor"
+        className="w-6 h-6"
+      >
+        <path
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          d="M20.25 6.375c0 2.278-3.694 4.125-8.25 4.125S3.75 8.653 3.75 6.375m16.5 0c0-2.278-3.694-4.125-8.25-4.125S3.75 4.097 3.75 6.375m16.5 0v11.25c0 2.278-3.694 4.125-8.25 4.125s-8.25-1.847-8.25-4.125V6.375m16.5 0v3.75m-16.5-3.75v3.75m16.5 0v3.75C20.25 16.153 16.556 18 12 18s-8.25-1.847-8.25-4.125v-3.75m16.5 0c0 2.278-3.694 4.125-8.25 4.125s-8.25-1.847-8.25-4.125"
+        />
+      </svg>
+    ),
   },
   {
     link: ROUTES.airdrop,
